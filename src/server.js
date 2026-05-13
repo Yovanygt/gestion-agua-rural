52d85f5819a3c231f4a064322be78ebcbe1f1cc6
Emaor <emanuelorozco1423@gmail.com>
2026-05-12 20:38:50 -0600

    feat: backend login y conexión MySQL

---
+++ src/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const app = require('./app');
+const db = require('./config/db');
+
+const PORT = process.env.PORT || 3000;
+
+async function iniciarServidor() {
+
+    try {
+
+        const connection = await db.getConnection();
+
+        console.log('MySQL conectado correctamente');
+
+        connection.release();
+
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en puerto ${PORT}`);
+        });
+
+    } catch (error) {
+
+        console.log('Error MySQL');
+        console.log(error);
+
+    }
+}
+
+iniciarServidor();
